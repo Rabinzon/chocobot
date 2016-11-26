fadbd7ab1fce9f69ada1fb3a87ff8190fc9779ed
rabinzon <ildarrazin@gmail.com>
2016-11-26 18:48:03 +0300

    feat(bot): add sendPrayerTime

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const getRates = require('./modules/getRates');
+const prayerTime = require('./modules/prayerTime');
 
 class Bot extends TelegramBot {
 	constructor(token, params) {
@@ -9,12 +10,18 @@ class Bot extends TelegramBot {
 	setListeners() {
 		this.onText(/^echo.?/, m => this.handler(m));
 		this.onText(/\$/, m => this.sendRates(m));
+		this.onText(/^🙏$/, m => this.sendPrayerTime(m));
 	}
 	getChatId(msg) {
 		return msg.chat.id;
 	}
 	handler(msg) {
 		this.sendMessage(this.getChatId(msg), 'I\'m alive! Whats up ? 🙈');
+	}
+	sendPrayerTime(msg) {
+		prayerTime('string').then(res => {
+			this.sendMessage(this.getChatId(msg), res);
+		});
 	}
 	sendRates(msg) {
 		getRates('string').then(res => {
