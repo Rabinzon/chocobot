9b4bfef3b38afc92f326419dcb2656e24abcf1ed
rabinzon <ildarrazin@gmail.com>
2016-11-29 10:12:37 +0300

    feat: add request handler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const Bot = require('./bot');
 const process = require('process');
+const express = require('express');
+const cors = require('cors');
 const token = process.env.TOKEN;
+const chatID = process.env.CHAT;
 const ip = process.env.IP;
+const server = express();
 
-new Bot(token, {polling: true}, ip);
+server.use(cors());
+
+const bot = new Bot(token, {polling: true}, ip, chatID);
+
+server.get('/', (req, res) => {
+	bot.sendToRootChat('bot updated 🚀');
+	res.send('OK');
+});
+
+server.listen(3091);
+
+
